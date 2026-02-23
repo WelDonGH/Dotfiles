073d101495464883d455da1bc58b24e0b796ebe3
WelDonGH <silvesterwr@gmail.com>
2026-02-23 14:58:58 +0300

    wtf i added those earlier

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,21 @@
+# pacman / yay aliases
+
+alias syu='yay -Syu'
+alias ss='yay -Ss'
+alias qs='yay -Qs'
+alias r='yay -R'
+alias s='yay -S'
+
+# misc
+
+alias la='ls -a'
+alias gc='git clone --depth=1'
+alias n='nvim'
+alias rm='rm -rf'
+alias hypr='nvim ~/.config/hypr/hyprland.conf'
+alias neofetch='fastfetch -c examples/13'
+alias cat='bat'
+alias vc='adguardvpn-cli connect -l FI'
+alias vd='adguardvpn-cli disconnect'
+alias downsort='bash ~/Scripts/downsort.sh'
+# alias zapret='bash ~/zapret-discord-youtube-linux/zapret.sh'
