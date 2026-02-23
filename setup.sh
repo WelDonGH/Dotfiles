d55564870b40dc28b77f6a9d7faffb35837a27e0
WelDonGH <silvesterwr@gmail.com>
2026-02-23 14:53:43 +0300

    setup.sh done

--- setup.sh
+++ setup.sh
@@ -2,10 +2,18 @@
 
 # hi
 
+# installing the essencials
 yay -Syu --noconfirm -needed alsa alsa-utils bat btop bc cava discord dunst ffmpeg grim gtk3 gtk4 hyprland iptables kitty man mesa neovim nftables nwg-look overskride pavucontrol pipewire pipewire-alsa pipewire-pulse portproton qbittorrent ranger rofi slurp sof-firmware steam telegram-desktop thunar tree-sitter nerd-fonts waybar wayland wireplumber wl-clipboard xdg-user-dirs zsh zen-browser-bin 7zip
 
-cp -r * $HOME/
+# installing ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-chsh /bin/zsh
+# cloning powerlevel10k
+cd ~
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# copying everything
+cd dotfiles
+cp * $HOME/
 
 reboot
