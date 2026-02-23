13e30450b42d0e9edb9c7a76b094a7e738d07102
WelDonGH <silvesterwr@gmail.com>
2026-02-23 15:28:26 +0300

    fucking hell

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # hi
 
 # installing the essencials
-yay -Syu --noconfirm -needed alsa alsa-utils bat btop bc cava discord dunst ffmpeg grim gtk3 gtk4 hyprland iptables kitty man mesa neovim nftables nwg-look overskride pavucontrol pipewire pipewire-alsa pipewire-pulse portproton qbittorrent ranger rofi slurp sof-firmware steam telegram-desktop thunar tree-sitter nerd-fonts waybar wayland wireplumber wl-clipboard xdg-user-dirs zsh zen-browser-bin 7zip
+yay -Syu --noconfirm -needed alsa alsa-utils bat btop bc cava discord dunst ffmpeg grim gtk3 gtk4 hyprland iptables kitty man mesa neovim nftables nwg-look overskride pavucontrol pipewire pipewire-alsa pipewire-pulse portproton qbittorrent ranger rofi slurp sof-firmware steam telegram-desktop thunar tree-sitter nerd-fonts waybar wayland wireplumber wl-clipboard xdg-user-dirs zsh zen-browser-bin 7zip udiskie
 
 # installing ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
