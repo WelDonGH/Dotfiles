ac90c2b184bd92ad17275b44159eb5d1853d0f75
WelDonGH <silvesterwr@gmail.com>
2026-02-23 13:27:01 +0300

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# hi
+
+yay -Syu --noconfirm -needed alsa alsa-utils bat btop bc cava discord dunst ffmpeg grim gtk3 gtk4 hyprland iptables kitty man mesa neovim nftables nwg-look overskride pavucontrol pipewire pipewire-alsa pipewire-pulse portproton qbittorrent ranger rofi slurp sof-firmware steam telegram-desktop thunar tree-sitter nerd-fonts waybar wayland wireplumber wl-clipboard xdg-user-dirs zsh zen-browser-bin 7zip
+
+cp -r * $HOME/
+
+chsh /bin/zsh
+
+reboot
